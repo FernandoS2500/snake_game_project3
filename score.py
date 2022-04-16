ec844695a96839fc348ce8979486dd8448f5204c
dudareolon <102680672+dudareolon@users.noreply.github.com>
2022-04-15 22:42:16 -0700

    Update score.py

--- score.py
+++ score.py
@@ -1,1 +1,26 @@
+"""
+File: score.py
+Team No.: 3
+Author name: Eduarda (Duda) Reolon
+Date completed: 04/18/2022
+Description: This program is a class for the score of a snake game.
+"""
 
+class Score:
+
+    def __init__(self, score: int):
+        """"Create instances from a class definition (__init__) for Score class"""
+        
+        self.score = score
+
+    def show_score():
+        """ """
+        print()
+
+    def increment_score():
+        """ """
+        print()
+
+    def game_over():
+        """ """
+        print()
