3920a94ed6c5dc35342b001ee36968fc4a1a41d9
dudareolon <102680672+dudareolon@users.noreply.github.com>
2022-04-18 18:26:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-
+"""
+File: main.py
+Team No.: 3
+Author name: Fernando Salazar, Michelle Bang, and Eduarda (Duda) Reolon
+Date completed: 04/21/2022
+Description: This program is the main code for a snake game that imports the classes Food, Snake and Score.
+"""
